3d71ae5c816919a298b8ebde6a4241cd7b8b557c
Shadman Sakib <5676838+shadmansakib@users.noreply.github.com>
2024-05-03 11:30:58 +0600

    minor refactor

--- todo-example-factory/main.ts
+++ todo-example-factory/main.ts
@@ -3,9 +3,9 @@ import { TodoServiceFactory } from './factory';
 export async function main() {
     const todoServiceFactory = new TodoServiceFactory();
     const todoService = todoServiceFactory.getNewObject('sql');
+    const todoList = await todoService.queryTodos();
     console.log('Todo list:');
-    console.log(await todoService.queryTodos());
-
+    console.log(todoList);
 }
 
 main();
